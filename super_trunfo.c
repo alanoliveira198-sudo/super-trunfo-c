851f60ed48e42e6b0bc31d94660aa48a8ac43d5e
alanoliveira198-sudo <alanoliveira198@gmail.com>
2025-10-13 14:11:53 +0000

    Update super_trunfo.c

--- super_trunfo.c
+++ super_trunfo.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
 
+typedef struct {
+    char estado;
+    char codigo[5];
+    char cidade[50];
+    int populacao;
+    float area_km2;
+    float pib_bilhoes;
+    int pontos_turisticos;
+} Carta;
+
 int main(void) {
-    printf("Base do projeto criada.\n");
+    printf("Estrutura Carta definida.\n");
     return 0;
 }
